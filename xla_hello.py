153be4ad358ae442817c236c12e85f74a8cd19e8
Nicholas Chaimov <nchaimov@paratools.com>
2025-03-25 22:24:26 +0000

    Change approach, try without torchrun

--- xla_hello.py
+++ xla_hello.py
@@ -1,24 +1,32 @@
 import sys
+import os
+import socket
+
+# PyTorch
 import torch
+import torch.distributed as dist
 
-# XLA imports
+# XLA (Accelerated Linear Algebra)
 import torch_xla.core.xla_model as xm
-import torch_xla.runtime as xlr
+import torch_xla.runtime as xr
 
-# Initialize XLA process group for torchrun
+# Distributed XLA
 import torch_xla.distributed.xla_backend
 
 def main():
-    world_size = xlr.world_size()
-    rank = xlr.global_ordinal()
-    torch.distributed.init_process_group('xla')
+    hostname = socket.gethostname()
+    rank = int(os.environ["SLURM_PROCID"])
+    world_size = int(os.environ["WORLD_SIZE"])
+    dist.init_process_group('xla', rank=rank, world_size=world_size)
+    if rank == 0: print(f"Group initialized? {dist.is_initialized()}", flush=True)
 
-    is_master = xm.is_master_ordinal()
+    xla_world_size = xlr.world_size()
+    xla_rank = xlr.global_ordinal()
+    xla_is_master = xm.is_master_ordinal()
 
-    args = sys.argv
+    print(f'{hostname}: Hello from {rank} of {world_size}, XLA rank {xla_rank} of {xla_world_size} {"MASTER" if xla_is_master else ""}')
 
-    print(f'Hello from {rank} of {world_size} {"MASTER" if is_master else ""}: argv={args}')
-
+    dist.barrier()
 
 if __name__ == '__main__':
     main()
