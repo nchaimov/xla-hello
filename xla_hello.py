175ae8045e59112487be902dcf38c0f054973225
Nicholas Chaimov <nchaimov@paratools.com>
2025-03-25 23:40:53 +0000

    Revert to torchrun, only srun one instance of torchrun and let torchrun launch the processes.

--- xla_hello.py
+++ xla_hello.py
@@ -6,18 +6,21 @@ import torch_xla.core.xla_model as xm
 import torch_xla.runtime as xlr
 
 # Initialize XLA process group for torchrun
+import torch.distributed as dist
 import torch_xla.distributed.xla_backend
 
 def main():
-    world_size = xlr.world_size()
-    rank = xlr.global_ordinal()
     torch.distributed.init_process_group('xla')
 
+    world_size = xlr.world_size()
+    rank = xlr.global_ordinal()
     is_master = xm.is_master_ordinal()
 
     args = sys.argv
 
     print(f'Hello from {rank} of {world_size} {"MASTER" if is_master else ""}: argv={args}')
+
+    dist.barrier()
 
 
 if __name__ == '__main__':
